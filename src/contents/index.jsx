a253add675d3ccfe9ce019450dcba27c98f34674
Jeff20151 <b09b02045@g.ntu.edu.tw>
2025-05-10 14:48:17 +0800

    add the path

--- src/contents/index.jsx
+++ src/contents/index.jsx
@@ -1,4 +1,4 @@
 export { default as Home } from './Home.jsx';
 export { default as Description } from './Descriptions.jsx';
-export { default as TeamMembers } from './teammembers.jsx';
+export { default as TeamMembers } from './TeamMembers.jsx';
 
