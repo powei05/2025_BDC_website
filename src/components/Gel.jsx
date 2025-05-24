c104b0def4e0df08c0d2b154177522a63cb7dd51
wei <willyjang20020504@gmail.com>
2025-05-24 22:14:24 +0800

    gel content design

--- src/components/Gel.jsx
+++ src/components/Gel.jsx
@@ -1,14 +1,27 @@
 import PropTypes from 'prop-types';
+import styles from './Gel.module.css';
+import hydrogelImg from '../../img/gel.jpg';
 export default function Gel({ onNext }) {
   return (
-    <div >
-      <button
-        
-        onClick={onNext}
-      >
-        GEL
-      </button>
-    </div>
+
+    <div className={styles.card}>
+      <div className={styles.left}>
+        <img src={hydrogelImg} alt="Hydrogel" className={styles.image} />
+        <h2 className={styles.title}>Hydrogel</h2>
+      </div>
+      <div className={styles.right}>
+        <p className={styles.description}>
+          Hydrogel is a network of polymer chains that are hydrophilic. They are often used in medical, agricultural, and wearable technologies.
+        </p>
+        <button
+          
+          onClick={onNext}
+          className={styles.button}>
+          GEL
+        </button>
+      </div>
+    </div>    
+
   );
 }
 Gel.propTypes = {
