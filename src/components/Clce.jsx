4c0336ea05d75c7a550ad5f20d109acb873d1231
wei <willyjang20020504@gmail.com>
2025-05-28 00:16:20 +0800

    tab redesign

--- src/components/Clce.jsx
+++ src/components/Clce.jsx
@@ -2,21 +2,31 @@
 import  { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Clce.module.css';
+import hydrogelImg from '../../img/gel.jpg';
+import cotton from '../../img/cotton.jpg';
 
 const tabs = [
   {
     id: 'tab-1',
     label: 'Work stuff',
+    pic: hydrogelImg,
+    pictxt: '圖片標題 2',
     content: (
       <>
         <p>Productize. Optics accountable talk. Thought shower. High performance keywords market-facing drink from the firehose, or you better eat a reality sandwich before you walk back in that boardroom, but accountable talk knowledge process outsourcing.</p>
         <p>What's our go to market strategy? cross functional teams enable out of the box brainstorming nor zeitgeist viral engagement. Deep dive. Organic growth quick sync, feed the algorithm.</p>
+         <p>Productize. Optics accountable talk. Thought shower. High performance keywords market-facing drink from the firehose, or you better eat a reality sandwich before you walk back in that boardroom, but accountable talk knowledge process outsourcing.</p>
+        <p>What's our go to market strategy? cross functional teams enable out of the box brainstorming nor zeitgeist viral engagement. Deep dive. Organic growth quick sync, feed the algorithm.</p>
+        <p>Productize. Optics accountable talk. Thought shower. High performance keywords market-facing drink from the firehose, or you better eat a reality sandwich before you walk back in that boardroom, but accountable talk knowledge process outsourcing.</p>
+        <p>What's our go to market strategy? cross functional teams enable out of the box brainstorming nor zeitgeist viral engagement. Deep dive. Organic growth quick sync, feed the algorithm.</p>     
       </>
     )
   },
   {
     id: 'tab-2',
     label: 'Food',
+    pic: hydrogelImg,
+    pictxt: '圖片標題 2',
     content: (
       <>
         <p>I love cheese, especially the big cheese gouda. Monterey jack red leicester roquefort cheese and wine fromage frais smelly cheese melted cheese dolcelatte. Fromage smelly cheese manchego paneer cheese and wine danish fontina macaroni cheese red leicester.</p>
@@ -27,6 +37,8 @@ const tabs = [
   {
     id: 'tab-3',
     label: 'Baking',
+    pic: cotton,
+    pictxt: '圖片標題 2',
     content: (
       <>
         <p>Cupcake ipsum dolor sit amet jujubes tart. Tiramisu icing gingerbread halvah cake. Marzipan cake soufflé cookie brownie ice cream cupcake. Dragée croissant bonbon ice cream oat cake jelly cookie. Wafer candy dessert jelly jelly-o.</p>
@@ -37,6 +49,8 @@ const tabs = [
   {
     id: 'tab-4',
     label: 'Cat',
+    pic: cotton,
+    pictxt: '圖片標題 2',
     content: (
       <>
         <p>Miaow then turn around and show you my bum flee in terror at cucumber discovered on floor. Terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not sorry sleep on dog bed, force dog to sleep on floor and grab pompom in mouth and put in water dish cats are fats i like to pets them they like to meow back present belly, scratch hand when stroked.</p>
@@ -62,18 +76,26 @@ export default function Clce({ onNext }) {
           </button>
         ))}
       </div>
+
       <div className={styles.content}>
         {tabs.map(tab => (
           <div
             key={tab.id}
             id={tab.id}
             className={styles.contentInner}
-            style={{ display: activeTab === tab.id ? 'block' : 'none' }}
+            style={{ display: activeTab === tab.id ? 'flex' : 'none' }}
           >
-            <div className={styles.page}>{tab.content}</div>
+            <div className={styles.leftPanel}>
+              <img src={tab.pic} alt={tab.pictxt} className={styles.image} />
+              <div className={styles.caption}>{tab.pictxt}</div>
+            </div>
+            <div className={styles.rightPanel}>
+              <div className={styles.page}>{tab.content}</div>
+            </div>
           </div>
         ))}
       </div>
+
       <button onClick={onNext}>COMPLETE</button>
     </div>
   );
